3e4ab1b87dc3079ef954fec1b87400ff5606e512
claramateooo <150123384+claramateooo@users.noreply.github.com>
2025-07-23 20:50:24 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+import express from "express";
+import puppeteer from "puppeteer";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/accesibilidad", async (req, res) => {
+  const { url } = req.body;
+  if (!url) return res.status(400).json({ error: "Falta URL" });
+
+  try {
+    const browser = await puppeteer.launch({
+    headless: "new",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle2" });
+
+    const snapshot = await page.accessibility.snapshot();
+    await browser.close();
+
+    res.json(snapshot);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+  console.log("Servidor accesibilidad en http://localhost:" + PORT);
+});
